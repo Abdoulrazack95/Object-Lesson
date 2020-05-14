71ec4dd1f50f9d3135f282a6af5344fe7d6757bb
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2020-05-14 19:16:30 +0100

    Demo and Exercises correction

---
+++ demo.js
@@ -0,0 +1,11 @@
+// Object literal syntax Demo 1  Phone, Cars
+
+//Accessing an Object’s property Demo 2 Phone, Cars
+
+//Adding a new Object's property Demo 3 Phone, Cars
+
+// Updating a value of an Object's property Demo 4 Phone, Cars
+
+// More about Object Phone, Cars
+
+// Object Methods
